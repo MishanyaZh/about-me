314a3427912e86fe62d7fed47784e26be7067b1b
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2024-11-16 10:42:06 +0100

    add CV pdf and update data education and work experience sections

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -36,9 +36,10 @@ export const LANGUAGES = [
 
 export const EDUCATION_EXPERIENCE = [
   {
-    subtitle: 'English School "Study-Less"',
-    date: '(August 2021 - June 2022)',
-    skills: ['Upper-Intermediate (B2)'],
+    subtitle:
+      'Cherkassy Branch of Private Higher Education Establishment "European University"',
+    date: '(2006 - 2011)',
+    skills: ['Finance'],
   },
   {
     subtitle: 'IT School "GoIT"',
@@ -46,54 +47,65 @@ export const EDUCATION_EXPERIENCE = [
     skills: ['Full Stack Developer (JS, React.js, Node.js, Html, CSS)'],
   },
   {
-    subtitle: 'Eduatut Policealna Szkola Zawodowa',
-    date: '(26.06.2020)',
+    subtitle: 'Policealna Szkoła Zawodowa "Eduatut"',
+    date: '(2018 - 2020)',
     skills: ['IT specialist'],
+  },
+];
+
+export const OTHER_EDUCATION_EXPERIENCE = [
+  {
+    subtitle: 'English School "Study-Less"',
+    date: '(August 2021 - June 2022)',
+    skills: ['Upper-Intermediate (B2)'],
   },
   {
     subtitle: '"Academy of language"',
-    date: '(05.01.2020)',
+    date: '(2019 - 2020)',
     skills: ['English language course'],
   },
   {
     subtitle: 'Course "Graphic designer"',
-    date: '(17.02.2019)',
+    date: '(2019)',
     skills: ['Photoshop, Corel'],
-  },
-  {
-    subtitle:
-      'Cherkassy Branch of Private Higher Education Establishment "European University"',
-    date: '(2006 - 2011)',
-    skills: ['Finance'],
   },
 ];
 
 export const WORK_EXPERIENCE = [
   {
-    subtitle: 'React Developer | "Digital Creative Cluster"',
-    date: '(Jan 2024 - present)',
+    subtitle: '"Learning-Space Sp. z o..o." | "Digital Creative Cluster"',
+    role: 'Frontend React Developer',
+    date: '(Jun 2022 - present)',
     skills: [
-      'Developed and maintained an educational platform using React, TypeScript, GraphQL, context, redux.',
-      'Enhanced and optimized UI components with Styled Components and Material UI.',
-      'Implemented optimizations for GraphQL, add hooks.',
-      'Conducting code reviews, and utilizing ClickUp for efficient task management and workflow visualization.',
-    ],
-  },
-  {
-    subtitle: 'React Developer | "Learning-Space Sp. z o..o."',
-    date: '(Jun 2022 - Jan 2024)',
-    skills: [
-      'Developed an interactive platform using React, Styled Components, and Material UI. ',
-      'Enhancing and optimized user interface components for better design consistency and performance.',
-      'Participated in team meetings and project planning using Slack and Google Meet.',
-      'Managed code versioning using Git and GitHub.',
+      'Process Optimization: Developed custom hooks and optimized code to avoid duplication, which reduced development time by 40% and streamlined scalability for future updates.',
+      'Feature Expansion & User Experience: Designed a multi-step course generator and payment flow generator, accelerating content creation for users by 4 times. Additionally, developed features for tracking student progress and displaying comprehensive course statistics, enabling instructors to monitor performance across individual and multiple courses.',
+      'Sales and Analytics: Developed components for sales tracking and data analytics, allowing users to view real-time statistics on product performance and customer engagement.',
+      'Content Management Solutions: Configured Webpack for front-end content storage (logos, regulations) used in email distribution, eliminating the need for paid third-party resources and saving company expenses.',
+      'Client Acquisition & Project Growth: Developed and delivered critical project features on time, which contributed to the acquisition of 3 new clients and investors, securing funding for further development.',
+      'Localization & User Customization: Implemented multi-language support (en, pl, it) and customized content spaces for different user needs, enhancing the platforms flexibility and expanding the target audience.',
+      'Enhanced Front-End Frameworks: Led the upgrade of Material UI from versions 4-5 to 6-7, enabling the use of the latest features and improving UI consistency across the application.',
+      'Team Collaboration & Quality Control: Conducted regular code reviews to maintain high-quality standards and prevent production issues, assisting new team members with onboarding and ensuring efficient integration into the development process.',
     ],
   },
   {
     subtitle: 'Developer experience',
     date: '(Jul 2020 - present)',
+    role: 'Full-Stack React Developer',
     skills: [
       'I create and deploy web applications, handling everything from database integration to user interface development, and also develop custom web solutions',
+    ],
+  },
+];
+
+export const OTHER_WORK_EXPERIENCE = [
+  {
+    subtitle: 'Faurecia"| "Forvia',
+    date: '(2018 - 2022)',
+    role: 'Operator',
+    skills: [
+      'I consistently met production targets and maintained high standards of quality and efficiency, resulting in a 20% decrease in the number of complaints.',
+      'Contributed to process improvements that increased output and resulted in a 10-20% decrease in defective products.',
+      'Collaborated effectively with team members to ensure smooth operations and adherence to safety protocols.',
     ],
   },
 ];
